c47c251949d3a0139e4a89f9c58862362e64ffed
unknown <delfdevs@gmail.com>
2019-12-16 00:08:55 -0500

    msgapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import SearchForm from "./components/SearchForm";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import LocationsList from "./components/LocationsList"
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage}/>
+      <Route  path="/characters" component={CharacterList}/>
+      <Route  path="/searchform" component={SearchForm}/>
+      <Route  path="/location" component={LocationsList}/>
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,72 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  
+  //const [characters, setCharacters] = useState([]);
+  const [foundValue, setFoundValue] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  
+  const searchOnChange = e => {
+    setSearchTerm(e.target.value);
+  }
+  
+  
+   useEffect(() => {
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+     .then(res => {
+      // setCharacters(res.data.results);
+      console.log(res.data.results);
+      setFoundValue(res.data.results);
+      setSearchResults(res.data.results)
+    })
+    .catch(e => {
+      console.log(e);
+      }); 
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+}, []);
 
+useEffect(() => {
+  const results = foundValue.filter(character => {
+    return character.name.toLowerCase().includes(searchTerm.toLowerCase());
+     });
+ 
+ 
+ setSearchResults(results)
+
+},[searchTerm])
+// end comment
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+      <form>
+        <input
+          name='name' 
+          className='search-form'
+          placeholder='Search by First or Last Name'
+          onChange={searchOnChange}
+          value={searchTerm}
+        />
+        {searchResults.map(character => (  // comment
+          <CharacterCard
+          id={character.id}
+          name={character.name}
+          species={character.species}
+          status={character.status}
+          image={character.image}
+          />   
+          ))
+      }
+     </form>
+      
+      
+      <section className="character-list">
+
+     
+      </section>
+    </div>
   );
-}
+    }
